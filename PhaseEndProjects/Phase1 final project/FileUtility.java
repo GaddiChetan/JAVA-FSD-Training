0931b0d9d6518f6bd368b842ae6885139266f92b
GaddiChetan <chetangaddi10@gmail.com>
2022-02-21 13:15:43 +0530

    Phase1 final project

---
+++ PhaseEndProjects/Phase1 final project/FileUtility.java
@@ -0,0 +1,11 @@
+package simpli.learn.finalprojectone;
+
+import java.io.FileNotFoundException;
+
+public interface FileUtility {
+	public void showAllFiles();
+	public void addFile();
+	public void deleteFile();
+	abstract void searchFile() throws FileNotFoundException;
+
+}
